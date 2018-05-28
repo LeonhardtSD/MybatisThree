e09bb3292524cf672a77a9e9a8bb9dfbb4c97aba
LeonhardtSD <leonhardtsd@163.com>
2018-05-28 14:23:18 +0800

    修改完毕，已正常运行

--- src/main/java/com/gaozhaoxi/mapper/UserMapper.java
+++ src/main/java/com/gaozhaoxi/mapper/UserMapper.java
@@ -1,8 +1,12 @@
 package com.gaozhaoxi.mapper;
 
 import com.gaozhaoxi.entity.User;
+import com.gaozhaoxi.transferBean.UserParams;
+import org.apache.ibatis.annotations.Param;
 
+import javax.jws.soap.SOAPBinding;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -11,5 +15,19 @@ import java.util.Map;
 public interface UserMapper {
     public User getUser(Long id);
 
+    //使用@Param注解 解决select多条件查询（即传递一个对象）
+//    在数据提交的过程中，mybatis会以@Param提供的名称为准，所以@Param（）中的名称要与userMapper.xml的参数对应上
+    public List<User> getUserByAddressAndName(@Param("username") String userName, @Param("address") String address);
+
+    //使用Map集合传递参数
     public ArrayList<User> getUserByAddressAndName2(Map<String,String> map);
+
+    //使用JavaBean传递参数
+    public ArrayList<User> getUserByAddressAndName3(UserParams params);
+
+    //插入数据(主键回填)
+    public int insertUser1(User user);
+
+    //插入数据（insert中主键自定义）
+    public int insertUser2(User user);
 }
--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,10 +1,12 @@
 import com.gaozhaoxi.db.DButil;
 import com.gaozhaoxi.entity.User;
 import com.gaozhaoxi.mapper.UserMapper;
-import lombok.Data;
+import com.gaozhaoxi.transferBean.UserParams;
+import org.apache.ibatis.jdbc.SQL;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import javax.xml.crypto.Data;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -51,4 +53,92 @@ public class Main {
             }
         }
     }
+
+
+    //
+    @Test
+    public void test4(){
+        SqlSession sqlSession=null;
+        try{
+            sqlSession=DButil.openSession();
+            UserMapper userMapper=sqlSession.getMapper(UserMapper.class);
+            List<User> users=userMapper.getUserByAddressAndName("gao","123");
+            System.out.println(users.toString());
+            sqlSession.commit();
+        }catch (Exception e){
+            e.printStackTrace();
+            sqlSession.rollback();
+        }finally {
+            if(sqlSession!=null){
+                sqlSession.close();
+            }
+        }
+    }
+
+
+    @Test
+    //使用javabean 来传递参数
+    public void test6(){
+        SqlSession sqlSession=null;
+        try{
+            sqlSession= DButil.openSession();
+            UserMapper userMapper=sqlSession.getMapper(UserMapper.class);
+
+            UserParams userParams=new UserParams("gao","123");
+            List<User> list=userMapper.getUserByAddressAndName3(userParams);
+            System.out.println(list);
+            sqlSession.commit();
+        }catch (Exception e){
+            e.printStackTrace();
+            sqlSession.rollback();
+        }finally {
+            if(sqlSession!=null){
+                sqlSession.close();
+            }
+        }
+    }
+
+//插入数据（主键回填）
+    @Test
+    public void test7(){
+        SqlSession sqlSession=null;
+        try{
+            sqlSession=DButil.openSession();
+            UserMapper userMapper=sqlSession.getMapper(UserMapper.class);
+            User user1=new User(null,"li0","456","789");
+            //myid 表示 受影响的行数。
+            int myid=userMapper.insertUser1(user1);
+            System.out.println("myid:"+myid+"|||||||"+"ID:"+user1.getId());
+            sqlSession.commit();
+        }catch (Exception e){
+            e.printStackTrace();
+            sqlSession.rollback();
+        }finally {
+            if(sqlSession!=null){
+                sqlSession.close();
+            }
+        }
+    }
+
+    @Test
+    public void test8(){
+        SqlSession sqlSession=null;
+        try{
+            sqlSession=DButil.openSession();
+            UserMapper userMapper=sqlSession.getMapper(UserMapper.class);
+            User user=new User(null,"gao","45454","7878");
+            //myid 表示 受影响的行数。
+            int myid=userMapper.insertUser2(user);
+            System.out.println("myid:"+myid+"|||||||"+"ID:"+user.getId());
+            sqlSession.commit();
+        }catch (Exception e){
+            e.printStackTrace();
+            sqlSession.rollback();
+        }finally {
+            if(sqlSession!=null){
+                sqlSession.close();
+            }
+        }
+    }
+
 }
